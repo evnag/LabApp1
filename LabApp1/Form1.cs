782aa5c5f6a7d48ddf9299615e19f43e071184fa
evnag <vskonev22@gmail.com>
2022-10-29 11:44:06 +0300

    Добавьте файлы проекта.

---
+++ LabApp1/Form1.cs
@@ -0,0 +1,31 @@
+namespace LabApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(300, 500);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form3();
+            form2.ShowDialog();
+        }
+    }
+}
